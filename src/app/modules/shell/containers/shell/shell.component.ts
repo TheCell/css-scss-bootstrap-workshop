37e91333f37bc7299653f6625bae751c42c842af
Tim Honermann <honermti@windowslive.com>
2024-09-01 10:06:04 +0200

    fix logo styling

--- src/app/modules/shell/containers/shell/shell.component.ts
+++ src/app/modules/shell/containers/shell/shell.component.ts
@@ -20,7 +20,7 @@ export class ShellComponent {
       routerLink: 'home'
     },
     {
-      label: 'Exercise',
+      label: 'Exercise with longer title',
       routerLink: 'exercise'
     }
   ]
